22e8027a27d919483d3dd16a55253dbd4c7a3e1a
Shonorov <shonorov@gmail.com>
2020-04-25 12:46:11 +0300

    Jump Game

--- src/main/com/company/challange/JumpGame.java
+++ src/main/com/company/challange/JumpGame.java
@@ -1,5 +1,9 @@
 package com.company.challange;
 
+enum Index {
+    GOOD, BAD, UNKNOWN
+}
+
 /**
  * Jump Game
  * https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/531/week-4/3310/
@@ -23,8 +27,8 @@ package com.company.challange;
  * Traverse range elements from end.
  * If max jump of range element lower than step max length - skip check.
  *
- * Time complexity : O(n).
- * Space complexity : O(0).
+ * Time complexity : O(2 ^ n).
+ * Space complexity : O(n). Recursion requires additional memory for the stack frames.
  */
 public class JumpGame {
 
@@ -39,7 +43,42 @@ public class JumpGame {
 
     }
 
+    // Greedy. Time complexity : O(n). Space complexity : O(1).
     private boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
+    // Dynamic programming Bottom-up. Use memo table storing element good or bad.
+    // Time complexity : O(n ^ 2). Space complexity : O(n).
+    public boolean canJump3(int[] nums) {
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
+    }
+
+
+    // Recursive Time complexity : O(2 ^ n).
+    private boolean canJump2(int[] nums) {
         return canFinish(nums, 0);
     }
 
@@ -55,4 +94,5 @@ public class JumpGame {
         }
         return false;
     }
+
 }
